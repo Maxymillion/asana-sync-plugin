8929569f1416fe666e1c979ec1e6349e5d72f831
Max van Essen <max.vessen@icloud.com>
2022-12-27 20:50:38 +0100

    Add Asana Search Modal

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,10 @@ interface AsanaSyncSettings {
 	asanaUserGID: string;
 	asanaWorkplaces: AsanaWorkspace[];
 	selectedWorkspace: string;
+	syncInterval: number;
+	maxShown: number;
+	searchShowDueDate: boolean;
+	searchShowProjectName: boolean;
 }
 
 interface AsanaWorkspace {
@@ -30,27 +34,48 @@ const DEFAULT_SETTINGS: AsanaSyncSettings = {
 	asanaAPIKey: '',
 	asanaUserGID: '',
 	asanaWorkplaces: '',
+	syncInterval: 0,
+	maxShown: 9,
+	searchShowDueDate: true,
 }
 
+interface AsanaModalSettings {
+	selected: string;
+	tasks: { data: [] };
+}
 
 export default class AsanaSync extends Plugin {
 	settings: AsanaSyncSettings;
 
+	asanaTasks = [];
 
-	async getTasksToday(workplace, assignee, key) {
+	async getTasks() {
+		if (this.settings.syncInterval === 0) {
+			new Notice(pluginName + ': retrieving from Asana...');
+			return await this.getTasksToday();
+		} else {
+			let tasks = this.asanaTasks;
+			return new Promise(function (resolve) {
+				resolve({data: tasks.data});
+			});
+		}
+	}
+
+	async getTasksToday() {
 		const response = await fetch('https://app.asana.com/api/1.0/tasks?' + new URLSearchParams({
 			opt_fields: 'due_on,name,projects',
 			opt_pretty: true,
-			workspace: workplace,
-			assignee: assignee,
+			workspace: this.settings.selectedWorkspace.gid,
+			assignee: this.settings.asanaUserGID,
 			completed_since: 'now'
 		}), {
 			method: 'GET',
 			headers: {
 				'Content-Type': 'application/json',
-				'Authorization': 'Bearer ' + key,
+				'Authorization': 'Bearer ' + this.settings.asanaAPIKey,
 			},
 		});
+
 		return response.json();
 	}
 
@@ -65,6 +90,18 @@ export default class AsanaSync extends Plugin {
 		return false;
 	}
 
+
+	isTomorrow(date) {
+		const tomorrow = new Date();
+		tomorrow.setDate(tomorrow.getDate() + 1);
+
+		if (tomorrow.toDateString() === date.toDateString()) {
+			return true;
+		}
+
+		return false;
+	}
+
 	async onload() {
 		await this.loadSettings();
 
@@ -73,131 +110,63 @@ export default class AsanaSync extends Plugin {
 			// Called when the user clicks the icon.
 			new Notice(pluginName + ': Not implemented');
 		});
-		// Perform additional things with the ribbon
+
 		ribbonIconEl.addClass('asana-sync-ribbon');
 
-		// // This adds a simple command that can be triggered anywhere
-		// this.addCommand({
-		// 	id: 'open-sample-modal-simple',
-		// 	name: 'Open sample modal (simple)',
-		// 	callback: () => {
-		// 		new SampleModal(this.app).open();
-		// 	}
-		// });
-		// // This adds an editor command that can perform some operation on the current editor instance
-		// this.addCommand({
-		// 	id: 'sample-editor-command',
-		// 	name: 'Sample editor command',
-		// 	editorCallback: (editor: Editor, view: MarkdownView) => {
-		// 		console.log(editor.getSelection());
-		// 		editor.replaceSelection('Sample Editor Command');
-		// 	}
-		// });
+		let syncInterval = window.setInterval(() => {
+			if (this.settings.syncInterval !== 0) {
+				// this.asanaTasks = this
+
+				this.getTasksToday().then((data) => {
+					this.asanaTasks = data;
+				})
+			}
+		}, 1000 * this.settings.syncInterval);
+
+		this.registerInterval(syncInterval);
+
 
 		this.addCommand({
 			id: 'asana-get-due-tasks',
 			name: 'Get Due Tasks',
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
-
-
 				let taskList = "";
-				await this.getTasksToday(this.settings.selectedWorkspace.gid, this.settings.asanaUserGID, this.settings.asanaAPIKey).then((res) => {
+				await this.getTasks().then((res) => {
 					res.data.map((task: any) => {
 						if (task.due_on !== "" && this.isToday(new Date(task.due_on))) {
 							let taskLink = "https://app.asana.com/0/" + (task.projects.length > 0 ? task.projects[0].gid : 0) + "/" + task.gid + "/f";
+
 							this.app.vault.create(task.gid + ".asana.md", taskTemplate(task.name, taskLink));
+
 							taskList += "- [ ] [[" + task.gid + ".asana|" + task.name + "]] \n";
 						}
 					});
 				});
-
 				editor.replaceSelection(taskList);
 			}
 		});
 
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		// this.addCommand({
-		// 	id: 'open-sample-modal-complex',
-		// 	name: 'Open sample modal (complex)',
-		// 	checkCallback: (checking: boolean) => {
-		// 		// Conditions to check
-		// 		const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-		// 		if (markdownView) {
-		// 			// If checking is true, we're simply "checking" if the command can be run.
-		// 			// If checking is false, then we want to actually perform the operation.
-		// 			if (!checking) {
-		// 				new SampleModal(this.app).open();
-		// 			}
-		//
-		// 			// This command will only show up in Command Palette when the check function returns true
-		// 			return true;
-		// 		}
-		// 	}
-		// });
+		this.addCommand({
+			id: 'asana-get-task',
+			name: 'Get Assigned Task',
+			checkCallback: (checking: boolean) => {
+				// Conditions to check
+				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				if (markdownView) {
+					// If checking is true, we're simply "checking" if the command can be run.
+					// If checking is false, then we want to actually perform the operation.
+					if (!checking) {
+						new AsanaSearchModal(this.app, this).open();
+					}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
+
+					// This command will only show up in Command Palette when the check function returns true
+					return true;
+				}
+			}
+		});
+
 		this.addSettingTab(new AsanaSyncSettingTab(this.app, this));
-
-		// // If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		// this.registerDomEvent(document, 'input', (evt) => {
-		// 	// // if(evt.inputType === "insertText"){
-		// 	// 	console.log('click', evt);
-		// 	// // }
-		//
-		//
-		// });
-		//
-
-
-		// this.app.vault.on("modify", (tel) => {
-		//
-		// 	// this.app.vault.read(tel).then((res) => {
-		// 	// 	res.split("\n").forEach((line, i) => {
-		// 	// 		if (line.replace(/\s/g, "").toLowerCase() === "%%asana%%"){
-		// 	// 			console.log("Found at "+i);
-		// 	// 		}
-		// 	// 	})
-		// 	// })
-		// });
-
-		// const getBullets = async () => {
-		// 	let taskList = "";
-		// 	await this.getTasksToday(this.settings.selectedWorkspace.gid, this.settings.asanaUserGID, this.settings.asanaAPIKey).then((res) => {
-		// 		res.data.map((task: any) => {
-		// 			if (task.due_on !== "" && this.isToday(new Date(task.due_on))) {
-		// 				taskList += "- [ ] [" + task.name + "](https://app.asana.com/0/" + (task.projects.length > 0 ? task.projects[0].gid : 0) + "/" + task.gid + "/f)  \n";
-		// 			}
-		// 		});
-		// 	});
-		//
-		// 	return taskList;
-		// }
-		//
-		// const findTag = () => {
-		// 	let startLine = 0;
-		// 	let endLine = 0;
-		//
-		// 	this.app.workspace.activeEditor?.editor?.getValue().split("\n").forEach((line, i) => {
-		// 		if (line.replace(/\s/g, "").toLowerCase() === "%%asana%%") {
-		// 			startLine = i;
-		// 		}
-		// 		if (line.replace(/\s/g, "").toLowerCase() === "%%asana-end%%") {
-		// 			endLine = i;
-		// 		}
-		// 	});
-		//
-		// 	getBullets().then((data) => {
-		// 		if (startLine > 0 && endLine < 1) {
-		// 			this.app.workspace.activeEditor?.editor?.setLine(startLine, "%% Asana %%\n\n"+data+"\n%% Asana-End %%");
-		// 		}else if(startLine > 0 && endLine > 0){
-		// 			this.app.workspace.activeEditor?.editor?.replaceRange("%% Asana %%\n\n"+data+"\n%% Asana-End %%\n", { line: startLine, ch: 0 }, {line:endLine+1, ch: 0} )
-		// 		}
-		// 	});
-		// }
-		//
-		// // // When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		// this.registerInterval(window.setInterval(() => findTag(), 2000));
 	}
 
 	onunload() {
@@ -213,14 +182,144 @@ export default class AsanaSync extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+class AsanaSearchModal extends Modal {
+
+	settings: AsanaModalSettings;
+	plugin: AsanaSync;
+
+	editor: Editor | undefined;
+
+	constructor(app: App, plugin: AsanaSync) {
 		super(app);
+		this.plugin = plugin;
+		this.editor = app.workspace.getActiveViewOfType(MarkdownView)?.editor;
+		this.settings = {selected: '', tasks: {data: []}};
+
+		this.modalEl.replaceChildren();
+		this.modalEl.addClass('asana-modal', 'prompt');
+		this.modalEl.removeClass('modal');
+		this.modalEl.tabIndex = -1;
+
+		this.scope.register([], 'ArrowDown', e => {
+			e.preventDefault()
+			this.arrowMoveSelected(this.modalEl, "down");
+		})
+		this.scope.register([], 'ArrowUp', e => {
+			e.preventDefault()
+			this.arrowMoveSelected(this.modalEl, "up");
+		})
+
+		this.scope.register([], 'Enter', e => {
+			e.preventDefault();
+			this.getTaskFromSelection();
+		})
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	private setSelected(target: any) {
+		let selectedClass = "is-selected";
+		this.settings.selected = target.getAttribute('data-gid');
+
+		let elements = target.parentElement.getElementsByClassName(selectedClass);
+
+		for (let element of elements) {
+			element.removeClass(selectedClass)
+		}
+
+		target.addClass(selectedClass);
+	}
+
+	private getParsedTask(task: any) {
+		let taskLink = "https://app.asana.com/0/" + (task.projects.length > 0 ? task.projects[0].gid : 0) + "/" + task.gid + "/f";
+
+		this.app.vault.create(task.gid + ".asana.md", taskTemplate(task.name, taskLink)).catch(() => {
+			console.log("File already exists, skipped creating file")
+		});
+
+		return "- [ ] [[" + task.gid + ".asana|" + task.name + "]] \n";
+	}
+
+	private getTaskFromSelection() {
+		if (this.settings.selected !== "") {
+
+			this.editor?.replaceSelection(this.getParsedTask(this.settings.tasks.data.filter((val) => val.gid === this.settings.selected)[0]));
+
+			this.close();
+		}
+	}
+
+	private arrowMoveSelected(modal: any, direction: string) {
+		let selectedEl = modal.querySelector('[data-gid="' + this.settings.selected + '"]');
+		let parentChildren = selectedEl.parentElement?.children;
+
+		if (direction === "down" && selectedEl.nextSibling !== null) {
+			this.setSelected(selectedEl.nextSibling);
+			selectedEl.nextSibling.scrollIntoView({block: "end", inline: "nearest"});
+		} else if (direction === "down") {
+			this.setSelected(parentChildren[0]);
+			parentChildren[0].scrollIntoView({block: "end", inline: "nearest"});
+		}
+
+		if (direction === "up" && selectedEl.previousSibling !== null) {
+			this.setSelected(selectedEl.previousSibling);
+			selectedEl.previousSibling.scrollIntoView({block: "end", inline: "nearest"});
+		} else if (direction === "up") {
+			this.setSelected(parentChildren[parentChildren.length - 1]);
+			parentChildren[parentChildren.length - 1].scrollIntoView({block: "end", inline: "nearest"});
+		}
+	}
+
+	private generateList(modalEl: any, tasks: any) {
+		let results = modalEl.createDiv('prompt-results');
+		tasks.map((task: any) => {
+			let item = results.createDiv('suggestion-item mod-complex')
+			item.setAttribute("data-gid", task.gid);
+			let el = item.createDiv('suggestion-content');
+			el.createDiv('suggestion-title').innerHTML = task.name;
+			if (task.due_on && this.plugin.settings.searchShowDueDate) {
+				let due = new Date(task.due_on);
+
+				el.createDiv('suggestion-due-date').innerHTML = "Due " + (!this.plugin.isToday(due) ? !this.plugin.isTomorrow(due) ? "on " + due.toLocaleDateString() : "Tomorrow" : "today");
+			}
+			item.onclick = () => this.getTaskFromSelection();
+			item.onmouseenter = (e: MouseEvent) => this.setSelected(e.target);
+		});
+
+		if (results.children.length > 0) {
+			this.settings.selected = results.children[0].getAttribute('data-gid');
+			results.children[0].addClass("is-selected");
+			results.style.maxHeight = (results.children[0].offsetHeight * this.plugin.settings.maxShown) + "px";
+		} else {
+			this.settings.selected = '';
+		}
+	}
+
+	async onOpen() {
+		const {modalEl} = this;
+
+		if(this.plugin.settings.syncInterval === 0) new Notice(pluginName + ': retrieving from Asana...');
+		this.settings.tasks = await this.plugin.getTasks();
+		if(this.plugin.settings.syncInterval === 0) new Notice(pluginName + ': retrieved ' + (this.settings.tasks.data.length) + ' task(s)');
+
+		let taskResults = this.settings.tasks;
+
+		let taskInput = modalEl.createDiv('prompt-input-container')
+			.createEl('input', 'prompt-input');
+
+		taskInput.placeholder = "Find using task name";
+
+		taskInput.oninput = (e: InputEvent) => {
+			if (e.target && e.target.value !== "") {
+				let resultEl = modalEl.querySelector(".prompt-results");
+				if (resultEl !== null) {
+					resultEl.remove();
+				}
+				this.generateList(modalEl, taskResults.data.filter((val) => val.name.toLowerCase().contains(e.target.value.toLowerCase())));
+			}
+		}
+
+		taskInput.focus();
+
+		this.generateList(modalEl, taskResults.data);
 	}
 
 	onClose() {
@@ -321,8 +420,25 @@ class AsanaSyncSettingTab extends PluginSettingTab {
 				}
 			)
 
-
-		console.log(this.plugin.settings);
+		new Setting(containerEl)
+			.setDisabled(true)
+			.setClass(this.plugin.settings.asanaWorkplaces.length < 1 ? "setting-invisible" : "setting-visible")
+			.setName('Sync Interval')
+			.setDesc('Select an interval in which the cache will update, immediate will cause the plugin to directly access the API on every call instead of the cached version')
+			.addDropdown((dd) => {
+					dd.addOption(String(0), "Immediate");
+					dd.addOption(String(1), "Every minute");
+					dd.addOption(String(5), "Every 5 minutes");
+					dd.addOption(String(15), "Every 15 minutes");
+					dd.addOption(String(30), "Every 30 minutes");
+					dd.addOption(String(60), "Every hour");
+					dd.onChange(async (val) => {
+						this.plugin.settings.syncInterval = Number(val);
+						await this.plugin.saveSettings();
+					})
+					return dd;
+				}
+			)
 
 		new Setting(containerEl)
 			.setName("Reset")
@@ -334,6 +450,42 @@ class AsanaSyncSettingTab extends PluginSettingTab {
 					this.plugin.settings = Object.assign({}, DEFAULT_SETTINGS);
 					await this.plugin.saveSettings();
 					this.display();
+				})
+			)
+
+		containerEl.createEl('h3', {text: "Search settings"})
+
+		new Setting(containerEl)
+			.setName('Max shown')
+			.setDesc('The maximum shown tasks before scrolling')
+			.addText(text => text
+				.setValue(String(this.plugin.settings.maxShown))
+				.onChange(async (value) => {
+					if (Number(value) > 0 && Number(value) < 100) {
+						this.plugin.settings.maxShown = Number(value);
+						await this.plugin.saveSettings();
+					}
+				}));
+
+		new Setting(containerEl)
+			.setName('Show due date')
+			.setDesc('Show due date while using the search function')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.searchShowDueDate)
+				.onChange(async (value) => {
+					this.plugin.settings.searchShowDueDate = value;
+					await this.plugin.saveSettings();
+				})
+			)
+
+		new Setting(containerEl)
+			.setName('Show project name')
+			.setDesc('Show project name while using the search function')
+			.addToggle(toggle => toggle
+				.setValue(this.plugin.settings.searchShowProjectName)
+				.onChange(async (value) => {
+					this.plugin.settings.searchShowProjectName = value;
+					await this.plugin.saveSettings();
 				})
 			)
 	}
